1777ebe34672c4de9d2dfe3f15db7923bc424011
Sjors Robroek <s.robroek@vxsan.com>
2020-11-19 11:54:53 +0400

    fixed variables

--- main.tf
+++ main.tf
@@ -1,65 +1,65 @@
 ## application groups
 
 resource "nsxt_policy_group" "dns" {
-  display_name = "app.dns.${var.product.product_name}.${var.product.environment}"
+  display_name = "app.dns.${var.product}.${var.environment}"
   criteria {
     ipaddress_expression {
-      ip_addresses = var.product.dns_ip
+      ip_addresses = var.dns_ip
     }
   }
-   count = length(var.product.dns_ip) != 0 ? 1 : 0  
+   count = length(var.dns_ip) != 0 ? 1 : 0  
 }
 
 resource "nsxt_policy_group" "ntp" {
-  display_name = "app.ntp.${var.product.product_name}.${var.product.environment}"
+  display_name = "app.ntp.${var.product}.${var.environment}"
   criteria {
     ipaddress_expression {
-      ip_addresses = var.product.ntp_ip
+      ip_addresses = var.ntp_ip
     }
   }
-   count = length(var.product.ntp_ip) != 0 ? 1 : 0  
+   count = length(var.ntp_ip) != 0 ? 1 : 0  
 }
 
 
 resource "nsxt_policy_group" "proxy" {
-  display_name = "app.proxy.${var.product.product_name}.${var.product.environment}"
+  display_name = "app.proxy.${var.product}.${var.environment}"
   criteria {
     ipaddress_expression {
-      ip_addresses = var.product.proxy_ip
+      ip_addresses = var.proxy_ip
     }
   }
-   count = length(var.product.proxy_ip) != 0 ? 1 : 0
+   count = length(var.proxy_ip) != 0 ? 1 : 0
 }
 
 
 resource "nsxt_policy_group" "syslog" {
-  display_name = "app.syslog.${var.product.product_name}.${var.product.environment}"
+  display_name = "app.syslog.${var.product}.${var.environment}"
   criteria {
     ipaddress_expression {
-      ip_addresses = var.product.syslog_ip
+      ip_addresses = var.syslog_ip
     }
   }
-  count = length(var.product.syslog_ip) != 0 ? 1 : 0
+  count = length(var.syslog_ip) != 0 ? 1 : 0
 }
 
 
 resource "nsxt_policy_group" "smtp" {
-  display_name = "app.ntp.${var.product.product_name}.${var.product.environment}"
+  display_name = "app.ntp.${var.product}.${var.environment}"
   criteria {
     ipaddress_expression {
-      ip_addresses = var.product.smtp_ip
+      ip_addresses = var.smtp_ip
     }
   }
-   count = length(var.product.smtp_ip) != 0 ? 1 : 0
+   count = length(var.smtp_ip) != 0 ? 1 : 0
 }
 
 resource "nsxt_policy_group" "ldap" {
-  display_name = "app.ldap.${var.product.product_name}.${var.product.environment}"
+  display_name = "app.ldap.${var.product}.${var.environment}"
   criteria {
     ipaddress_expression {
-      ip_addresses = var.product.smtp_ip
+      ip_addresses = var.smtp_ip
     }
   }
-   count = length(var.product.ldap_ip) != 0 ? 1 : 0
+   count = length(var.ldap_ip) != 0 ? 1 : 0
 }
 
